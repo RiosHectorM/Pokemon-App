9c4d83959462cdca7b280fbf773445f1b5d7f410
RiosHectorM <rios.hector.martin@gmail.com>
2023-03-16 23:55:33 -0300

    "Object Pokemon OK"

--- api/src/controlers/getApiData.js
+++ api/src/controlers/getApiData.js
@@ -4,26 +4,6 @@ const axios = require('axios');
 const getPokeApi = async () => {
   const pokemons = await axios('https://pokeapi.co/api/v2/pokemon');
 
-  // const mapUrl = await pokemons.data.results.map((e) => {
-  //   return e.url;
-  // });
-  // var arrayPokemones = [];
-  // for (var i = 0; i < mapUrl.length; i++) {
-  //   const url = await axios(mapUrl[i]);
-  //   arrayPokemones.push({
-  //     idPoke: url.data.id,
-  //     name: url.data.name,
-  //     height: url.data.height,
-  //     weight: url.data.weight,
-  //     hp: url.data.stats.find((e) => e.stat.name === 'hp').base_stat,
-  //     attack: url.data.stats.find((e) => e.stat.name === 'attack').base_stat,
-  //     defense: url.data.stats.find((e) => e.stat.name === 'defense').base_stat,
-  //     speed: url.data.stats.find((e) => e.stat.name === 'speed').base_stat,
-  //     types: url.data.types.map((e) => (e = { name: e.type.name })),
-  //     // img: url.data.sprites.versions['generation-v']['black-white'].animated.front_default,
-  //     img: url.data.sprites.other['official-artwork'].front_default,
-  //   });
-  //}
   return pokemons;
 };
 
--- api/src/routes/pokeRouter.js
+++ api/src/routes/pokeRouter.js
@@ -14,13 +14,32 @@ router.get('/', async (req, res) => {
   let pokeArray = [];
   const pokeCant = 60;
   let resultApi = await pokeLoad('https://pokeapi.co/api/v2/pokemon');
-  
+
   while (pokeArray.length < pokeCant) {
     pokeArray = pokeArray.concat(resultApi.results);
     resultApi = await pokeLoad(resultApi.next);
-   }
+  }
   console.log(pokeArray.length);
-  
+  pokeArray.map(async (poke) => {
+    const pokeData = await axios(poke.url);
+    const pokemon = {
+      idPoke: pokeData.data.id,
+      name: pokeData.data.name,
+      image: pokeData.data.sprites.other.home.front_default,
+      hp: pokeData.data.stats[0].base_stat,
+      attack: pokeData.data.stats[1].base_stat,
+      defense: pokeData.data.stats[2].base_stat,
+      speed: pokeData.data.stats[5].base_stat,
+      height: pokeData.data.stats[5].base_stat,
+      weight: pokeData.data.stats[5].base_stat,
+      types: [
+        pokeData.data.types[0].type.name,
+        pokeData.data.types[1]?.type.name,
+      ],
+    };
+    console.log(pokemon);
+  });
+
   res.send(pokeArray);
 });
 
